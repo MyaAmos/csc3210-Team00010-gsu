6852e694302f7e290547efcdd1bcd27a440ea837
mfilik <60410506+mfilik@users.noreply.github.com>
2020-02-20 07:54:48 -0500

    Added project 2 files

---
+++ Project_2/m_f/second.s
@@ -0,0 +1,18 @@
+@ second program: c = a + b
+.section .data
+a: .word 2	@ 32-bit variable a in memory
+b: .word 5	@ 32-bit variable b in memory
+c: .word 0	@ 32-bit variable c in memory
+.section .text
+.globl _start
+_start:
+ldr r1, =a	@ load the memory address of a into r1
+ldr r1, [r1]	@ load the value a into r1
+ldr r2, =b	@ load the memory address of b into r2
+ldr r2, [r2]	@ load the value b into r2
+add r1, r1, r2	@ add r1 to r2 and store into r1
+ldr r2, =c	@ load the memory address of c into r2
+str r1, [r2]	@ store r1 into memory c
+mov r7, #1	@ Program Termination: exit syscall
+svc #0		@ Program Termination: wake kernel
+.end
---
+++ Project_2/m_f/spmd2.c
@@ -0,0 +1,31 @@
+/* spmd2.c
+* ... illustrates the SPMD pattern in OpenMP,
+* using the commandline arguments
+* to control the number of threads.
+*
+* Joel Adams, Calvin College, November 2009.
+*
+* Usage: ./spmd2 [numThreads]
+*
+* Exercise:
+* - Compile & run with no commandline args
+* - Rerun with different commandline args
+*/
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+int main(int argc, char** argv) {
+//int id, numThreads;
+printf("\n");
+if (argc > 1) {
+omp_set_num_threads( atoi(argv[1]) );
+}
+#pragma omp parallel
+{
+int id = omp_get_thread_num();
+int numThreads = omp_get_num_threads();
+printf("Hello from thread %d of %d\n", id, numThreads);
+}
+printf("\n");
+return 0;
+}
